51f81f7a7263ee18bfa368fe310c2dc849f19f30
Arnold Chand <arnold.chand@gmail.com>
2019-10-02 17:08:01 -0400

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'boootstrap';
+import 'bootstrap';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
