2d6ce95144f2870188d7c266af25bd9039bb4b6f
Arnold Chand <arnold.chand@gmail.com>
2019-10-01 23:45:51 -0400

    remove unwanted styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './Header.css';
 
 function Header() {
   return (
     <div className="Header">
-      <Row>
-        <Col>Settings</Col>
-        <Col>Help</Col>
-        <Col>Fullscreen</Col>
-      </Row>
     </div>
   );
 }
