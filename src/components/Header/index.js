f2be93f83ad27f07ed51d60038458c6effc35c74
Arnold Chand <arnold.chand@gmail.com>
2019-10-02 17:09:07 -0400

    add header content, incomplete

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,23 @@ import './style.scss';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        Header Component
+      <div className="header container-fluid p-0">
+        <div className="row">
+          <div className="col text-left">
+            <h2>
+              <a className="header__link" href="#">
+                About
+              </a>
+            </h2>
+          </div>
+          <div className="col text-right">
+            <h2>
+              <a className="header__link" href="#">
+                Fullscreen
+              </a>
+            </h2>
+          </div>
+        </div>
       </div>
     );
   }
