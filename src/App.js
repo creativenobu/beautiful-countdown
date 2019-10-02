abb7cfd2817556903a245ad6a8acaba44011a1f3
Arnold Chand <arnold.chand@gmail.com>
2019-10-02 00:00:56 -0400

    set app title in App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import './App.css';
 
-import Header from './components/Header/index';
-import Countdown from './components/Countdown/index';
-import Footer from './components/Footer/index';
+import Header from './components/Header';
+import Countdown from './components/Countdown';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Countdown/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    document.title = 'Beautiful Countdown';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Countdown/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
